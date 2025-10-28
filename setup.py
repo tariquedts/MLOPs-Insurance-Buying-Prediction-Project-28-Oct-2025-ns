6b3fd19c574def52179e2d363a695b65b967c07f
tarique anwar <tarique.dts@gmail.com>
2025-10-28 15:41:45 +0530

    my app is ready working fine locally

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Tarique Anwar",
+    author_email="tarique.dts@gmail.com",
+    packages=find_packages()
+)
